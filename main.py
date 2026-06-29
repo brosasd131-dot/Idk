136e9c8ce525cf4c21b98aa8c479d29284dd3d1c
Drchickengunpro2 <brosasd131@gmail.com>
2026-06-29 09:34:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+username='asrbuntuadmin11'
+password='helloadmin'
+usernameget=input('hi ceo type your username please ceo-:')
+passwordget=input('correct ceo please type your password-:')
+if usernameget==username:
+    if passwordget==password:
+      print('hello owner welcome back to asrbuntu ceo')
+
+    else:
+        print("fak you bradar ur not drchickengun")
+
+else:
+    print("BRADAR GO AWAY FAK YOU BRADAR")
+
